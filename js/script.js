9aaa422d98f4ed1d7d0d4297a18b05e74c65c007
rusemite <imangazinr0304@gmail.com>
2024-10-08 23:57:37 +0500

    Interim Upgrade

--- js/script.js
+++ js/script.js
@@ -1,8 +1,55 @@
-<<<<<<< HEAD
-document.getElementById("burger2").addEventListener("click", function(){
-    document.querySelector(".navbar").classList.toggle("open")
-=======
-document.getElementById("burger2").addEventListener("click", function(){
-    document.querySelector(".navbar").classList.toggle("open")
->>>>>>> 022ebc9257949a53594ec3943f370234a60928f6
-})
+document.getElementById("burger2").addEventListener("click", function () {
+  document.querySelector(".navbar").classList.toggle("open");
+});
+
+const plus = document.getElementById("butplus");
+const minus = document.getElementById("butminus");
+const mult = document.getElementById("butmult");
+const div = document.getElementById("butdiv");
+
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+
+const result = document.getElementById("result");
+
+let res = 0;
+
+function calculus(mark) {
+  const num1 = parseFloat(first.value) || 0;
+  const num2 = parseFloat(second.value) || 0;
+  switch (mark) {
+    case "+":
+      res = num1 + num2;
+      break;
+    case "-":
+      res = num1 - num2;
+      break;
+    case "*":
+      res = num1 * num2;
+      break;
+    case "/":
+      res = num2 !== 0 ? num1 / num2 : "error";
+      break;
+  }
+  result.textContent = res;
+}
+
+plus.addEventListener("click", (event) => {
+  event.preventDefault();
+  calculus("+");
+});
+
+minus.addEventListener("click", (event) => {
+  event.preventDefault();
+  calculus("-");
+});
+
+mult.addEventListener("click", (event) => {
+  event.preventDefault();
+  calculus("*");
+});
+
+plus.addEventListener("click", (event) => {
+  event.preventDefault();
+  calculus("/");
+});
