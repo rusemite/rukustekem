18a0a03ed8e55d15add07b6c20ea3292ba2b4df9
rusemite <imangazinr0304@gmail.com>
2024-10-10 12:01:56 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,16 @@ const plus = document.getElementById("butplus");
 const minus = document.getElementById("butminus");
 const mult = document.getElementById("butmult");
 const div = document.getElementById("butdiv");
+const sin = document.getElementById("butsin");
+const cos = document.getElementById("butcos");
+const tg = document.getElementById("buttg");
+const ctg = document.getElementById("butctg");
 
 let first = document.getElementById("first");
 let second = document.getElementById("second");
-
+let variable = document.getElementById("variable");
 const result = document.getElementById("result");
-
+const angle = document.getElementById("angle");
 let res = 0;
 
 function calculus(mark) {
@@ -34,22 +38,61 @@ function calculus(mark) {
   result.textContent = res;
 }
 
-plus.addEventListener("click", (event) => {
-  event.preventDefault();
+plus.addEventListener("click", (Event) => {
+  Event.preventDefault();
   calculus("+");
 });
-
-minus.addEventListener("click", (event) => {
-  event.preventDefault();
+minus.addEventListener("click", (Event) => {
+  Event.preventDefault();
   calculus("-");
 });
-
-mult.addEventListener("click", (event) => {
-  event.preventDefault();
+mult.addEventListener("click", (Event) => {
+  Event.preventDefault();
   calculus("*");
 });
-
-plus.addEventListener("click", (event) => {
-  event.preventDefault();
+div.addEventListener("click", (Event) => {
+  Event.preventDefault();
   calculus("/");
+});
+
+let val = 0;
+function trigonometric(operation) {
+  const num3 = parseFloat(variable.value) || 0;
+  num = (num3 * Math.PI) / 180;
+
+  switch (operation) {
+    case "sin":
+      val = Math.sin(num).toFixed(2);
+      break;
+    case "cos":
+      val = Math.cos(num).toFixed(2);
+      break;
+    case "tg":
+      val =
+        Math.abs(num % Math.PI) === Math.PI / 2
+          ? "error"
+          : Math.tan(num).toFixed(2);
+      break;
+    case "ctg":
+      val = num % Math.PI === 0 ? "error" : (1 / Math.tan(num)).toFixed(2);
+      break;
+  }
+  angle.textContent = val;
+}
+
+sin.addEventListener("click", (Event) => {
+  Event.preventDefault();
+  trigonometric("sin");
+});
+cos.addEventListener("click", (Event) => {
+  Event.preventDefault();
+  trigonometric("cos");
+});
+tg.addEventListener("click", (Event) => {
+  Event.preventDefault();
+  trigonometric("tg");
+});
+ctg.addEventListener("click", (Event) => {
+  Event.preventDefault();
+  trigonometric("ctg");
 });
